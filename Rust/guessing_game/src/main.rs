2fb125d69c2e1aaef14dd991028cc3e1c02517b9
Dunocat639YT <francescpasto@gmail.com>
2026-05-07 12:44:31 +0200

    .

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -8,23 +8,34 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    //println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Type a number!");
+                continue;
+            }
+        };
 
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("To small!");
-        Ordering::Equal => println!("You win!");
-        Ordering::Greater => println!("To big!");
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                },
+                Ordering::Greater => println!("Too big!"),
+            }
     }
+
+
 }
